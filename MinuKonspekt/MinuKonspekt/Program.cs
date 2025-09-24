c7632de82e3e7783957ef310114fdd034b2872e3
Opilane TTHK <opilane@tthk.ee>
2025-09-24 10:51:26 +0300

    if else if else
    kirutasin nimed

---
+++ MinuKonspekt/MinuKonspekt/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel.Design;
+
+namespace MinuKonspekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //Muutujad, Matemaatilised tehted, IF, ELSE, IF ja ELSE
+
+            //1.tuvasta sisu võrdluses tühja stringiga andmetüüp
+            Console.WriteLine("Tere kasutaja, mis on sinu nimi");
+            string nimi = Console.ReadLine();
+
+            if (nimi == "")
+            {
+                Console.WriteLine("Kasutaja ei sisestanud oma nime");
+            }
+            else if (nimi != "")
+            {
+                Console.WriteLine("Tere " + nimi + "!");
+            }
+            else
+            {
+                Console.WriteLine("Kasutaja ei sisestanud oma nime");
+            }
+
+            //2.tuvasta arvuvahemik, võrdluses piirväärtustega, int/double/float andmetüüp
+            Console.WriteLine(nimi + ", mis on sinu vanus?: ");
+            int kasutajavanus = int.Parse(Console.ReadLine());
+
+            //2.1 mitu tingimust pesastatud if-ide abil
+            //if (kasutajavanus > 0)
+            //{
+            //    if (kasutajavanus < 18)
+            //    {
+            //        Console.WriteLine("Kahjuks energiajooki sulle ei müüda, oled Monsterist ilma😁");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Saad osta Monsterit, YIPPIE 🍕");
+            //    }
+            //}
+
+            //2.2 mitu tingimust ühe ifi kasutades loogilist tehet "and", vältides pesastamist
+            if (kasutajavanus > 0 && kasutajavanus < 18)
+            {
+                Console.WriteLine("Kahjuks energiajooki sulle ei müüda, oled Monsterist ilma😁");
+            }
+            else
+            {
+                Console.WriteLine("Saad osta Monsterit, YIPPIE");
+            }
+            //2.3 mitu vahemikku, if-elseif abil, double/float
+            Console.WriteLine("Sisesta oma pikkus ka, palun, "+nimi+":");
+            double pikkus = double.Parse(Console.ReadLine());
+            if (pikkus < 1.00d)
+            {
+                Console.WriteLine("Oled juhtsu");
+            }
+            else if (pikkus <1.25d && pikkus > 1.00d)
+            {
+                Console.WriteLine("Oled peaaegu allameetrimees");
+            }
+            else if (pikkus <1.50d && pikkus >1.25d)
+            {
+                Console.WriteLine("Oioi, päkapikk enam ei olegi " + nimi + " !");
+            }
+            else if (pikkus < 1.75d && pikkus > 1.50d)
+            {
+                Console.WriteLine("Oioi, päkapikk enam ei olegi " + nimi + " !");
+            }
+            else if (pikkus < 2.00d && pikkus > 1.75d)
+            {
+                Console.WriteLine("Pikk kolge, vaata et sa pead vastu ukspiita ära ei löö");
+            }
+            else if (pikkus > 2.00d)
+            {
+                Console.WriteLine("Täielik tulnukas, kuidas pilved välja näevad?");
+            }
+            else
+            {
+                Console.WriteLine("pikkust ei tunta");
+            }
+        }
+    }
+}
+ 
