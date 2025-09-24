a0db66f3b8703457d048248f6c63ff396d8bf7a7
Opilane TTHK <opilane@tthk.ee>
2025-09-24 10:54:43 +0300

    lisadi kalkulator

--- MinuKonspekt/MinuKonspekt/Program.cs
+++ MinuKonspekt/MinuKonspekt/Program.cs
@@ -84,7 +84,76 @@ namespace MinuKonspekt
             {
                 Console.WriteLine("pikkust ei tunta");
             }
+
+            //int lahutav1 = 0;
+            //int lahutav2 = 0;
+            //Console.WriteLine("Tere, palun sisesta esimene lahutav: ");
+            //lahutav1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Tere, palun sisesta teine lahutav: ");
+            //lahutav2 = int.Parse(Console.ReadLine());
+            //int lahutav = lahutav1 - lahutav2;
+            //Console.WriteLine($"Tehte tulemus: {lahutav}");
+            //Console.WriteLine("Tehte tulemus: " + lahutav);
+
+            //int jagatav1 = 0;
+            //int jagatav2 = 0;
+            //Console.WriteLine("Tere, palun sisesta esimene jagatav: ");
+            //jagatav1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Tere, palun sisesta teine jagatav: ");
+            //jagatav2 = int.Parse(Console.ReadLine());
+            //int jagatav = jagatav1 / jagatav2;
+            //Console.WriteLine($"Tehte tulemus: {jagatav}");
+            //Console.WriteLine("Tehte tulemus: " + jagatav);
+
+            //int korrutav1 = 0;
+            //int korrutav2 = 0;
+            //Console.WriteLine("Tere, palun sisesta esimene korrutav: ");
+            //korrutav1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Tere, palun sisesta teine korrutav: ");
+            //korrutav2 = int.Parse(Console.ReadLine());
+            //int korrutav = korrutav1 * korrutav2;
+            //Console.WriteLine($"Tehte tulemus: {korrutav}");
+            //Console.WriteLine("Tehte tulemus: "+korrutav);
+
+            int arv1 = 0;
+            int arv2 = 0;
+            Console.WriteLine("Tere, palun sisesta esimene arv: ");
+            arv1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Palun sisesta teine arv: ");
+            arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - / *");
+            string tehtetüüp = Console.ReadLine();
+            int tehe = 0;
+
+            if (tehtetüüp == "+")
+            {
+                tehe = arv1 + arv2;
+            }
+
+            if (tehtetüüp == "-")
+            {
+                tehe = arv1 - arv2;
+            }
+
+            if (tehtetüüp == "/")
+            {
+                tehe = arv1 / arv2;
+            }
+
+            if (tehtetüüp == "*")
+            {
+                tehe = arv1 * arv2;
+            }
+
+            Console.WriteLine($"Tehte tulemus: {tehe}");
+
+
+
+            // + liitmine
+            // - lahutame
+            // / jagamine
+            // * korrutamine2
+
         }
     }
 }
- 
